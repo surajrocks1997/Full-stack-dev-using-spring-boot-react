3a6acf5f16fdfeabed23a1cb3a953e63789079c7
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-18 13:39:37 +0530

    location service implemented

--- locationweb/src/main/java/com/location/repos/LocationRepository.java
+++ locationweb/src/main/java/com/location/repos/LocationRepository.java
@@ -1,9 +1,9 @@
 package com.location.repos;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.location.entities.Location;
 
-public interface LocationRepository extends CrudRepository<Location, Integer> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
 
 }
---
+++ locationweb/src/main/java/com/location/service/LocationServiceImpl.java
@@ -0,0 +1,51 @@
+package com.location.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.location.entities.Location;
+import com.location.repos.LocationRepository;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+
+	@Autowired
+	private LocationRepository repository;
+	
+	@Override
+	public Location saveLocation(Location location) {
+		return repository.save(location);
+	}
+
+	@Override
+	public Location updateLocation(Location location) {
+		return repository.save(location);
+	}
+
+	@Override
+	public void deleteLocation(Location location) {
+		repository.delete(location);
+		
+	}
+
+	@Override
+	public Location getLocationById(int id) {
+		 return repository.findById(id).get();
+	}
+
+	@Override
+	public List<Location> getAllLocations() {
+		return repository.findAll();
+	}
+
+	public LocationRepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(LocationRepository repository) {
+		this.repository = repository;
+	}
+
+}
