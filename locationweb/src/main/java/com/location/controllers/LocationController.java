802479509dc526af2f30c8d03a59f07cb3db87cc
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-18 15:50:18 +0530

    controller added. post action done

--- locationweb/src/main/java/com/location/controllers/LocationController.java
+++ locationweb/src/main/java/com/location/controllers/LocationController.java
@@ -1,13 +1,30 @@
 package com.location.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.location.entities.Location;
+import com.location.service.LocationService;
 
 @Controller
 public class LocationController {
 
+	@Autowired
+	LocationService service;
+	
 	@RequestMapping("/showCreate")
 	public String showCreate() {
+		return "createLocation";
+	}
+	
+	@RequestMapping("/saveLoc")
+	public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
+		Location locationSaved = service.saveLocation(location);
+		String msg = "Location Saved with id: " + locationSaved.getId();
+		modelMap.addAttribute("msg", msg);
 		return "createLocation";
 	}
 }
--- locationweb/src/main/webapp/WEB-INF/jsps/createLocation.jsp
+++ locationweb/src/main/webapp/WEB-INF/jsps/createLocation.jsp
@@ -8,17 +8,19 @@
 </head>
 <body>
 
-<form action="" method="">
+<form action="saveLoc" method="post">
 <pre>
 Id: <input type="text" name="id"/>
 Code: <input type="text" name="code"/>
 Name: <input type="text" name="name"/>
-Type: Urban <input type="radio" name="type"/>
-Rural <input type="radio" name="type"/>
+Type: Urban <input type="radio" name="type" value="URBAN"/>
+Rural <input type="radio" name="type" value="RURAL"/>
 
 <input type="submit" value="save"/>
 </pre>
 </form>
+
+${msg}
 
 </body>
 </html>
