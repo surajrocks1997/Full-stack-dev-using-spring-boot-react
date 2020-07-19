aa8c89b558b3b2cfa4cc8ee41c3c0d26143c1714
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-19 10:43:39 +0530

    entities/model and relationship created

---
+++ flightreservation/src/main/java/com/flightreservation/entities/Reservation.java
@@ -0,0 +1,41 @@
+package com.flightreservation.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Reservation extends AbstractEntity {
+	
+	private Boolean checkIn;
+	private int numberOfBags;
+	@OneToOne
+	private Passenger passenger;
+	@OneToOne
+	private Flight flight;
+
+	public Boolean getCheckIn() {
+		return checkIn;
+	}
+	public void setCheckIn(Boolean checkIn) {
+		this.checkIn = checkIn;
+	}
+	public int getNumberOfBags() {
+		return numberOfBags;
+	}
+	public void setNumberOfBags(int numberOfBags) {
+		this.numberOfBags = numberOfBags;
+	}
+	public Passenger getPassenger() {
+		return passenger;
+	}
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+	
+}
